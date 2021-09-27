336397cc67e91432ddab4c5e8a3ef483073ede5b
Irtaza Chohan <irtazachohan@gmail.com>
2021-09-27 11:57:22 +0100

    Calculate number of words if entered as a nuber and also if entered on a textbox as free text.

--- wordcountcost/Form1.Designer.cs
+++ wordcountcost/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace wordcountcost
 {
-    partial class Form1
+    partial class FrmWordCount
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,13 +29,93 @@ namespace wordcountcost
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtWords = new System.Windows.Forms.TextBox();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtfullwords = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtWords
+            // 
+            this.txtWords.Location = new System.Drawing.Point(163, 37);
+            this.txtWords.Name = "txtWords";
+            this.txtWords.Size = new System.Drawing.Size(100, 23);
+            this.txtWords.TabIndex = 0;
+            this.txtWords.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.AutoSize = true;
+            this.lblInfo.Location = new System.Drawing.Point(152, 63);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(128, 15);
+            this.lblInfo.TabIndex = 1;
+            this.lblInfo.Text = "Enter number of words";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(163, 153);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(100, 36);
+            this.btnCalculate.TabIndex = 2;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(152, 209);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 15);
+            this.lblResult.TabIndex = 3;
+            this.lblResult.Visible = false;
+            // 
+            // txtfullwords
+            // 
+            this.txtfullwords.Location = new System.Drawing.Point(163, 94);
+            this.txtfullwords.Name = "txtfullwords";
+            this.txtfullwords.Size = new System.Drawing.Size(100, 23);
+            this.txtfullwords.TabIndex = 4;
+            this.txtfullwords.TextChanged += new System.EventHandler(this.txtfullwords_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(152, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Enter text to calculate";
+            // 
+            // FrmWordCount
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(446, 267);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtfullwords);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.txtWords);
+            this.Name = "FrmWordCount";
+            this.Text = "Word Count Cost";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtWords;
+        private System.Windows.Forms.Label lblInfo;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtfullwords;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- wordcountcost/Form1.cs
+++ wordcountcost/Form1.cs
@@ -10,12 +10,73 @@ using System.Windows.Forms;
 
 namespace wordcountcost
 {
-    public partial class Form1 : Form
+    public partial class FrmWordCount : Form
     {
-        public Form1()
+        public FrmWordCount()
         {
             InitializeComponent();
         }
 
+        //Assume that the cost, not including tax, for sending a telegram from Hamilton to Montreal is $4.50 for the first twenty
+        //words or less, plus $.17 for each additional word beyond twenty.Write a program which accepts the number of words
+        //and then determines the total cost.
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        double words = 0;
+        double result;
+        string fullwords;
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //caluclate cost of number of words entered in text box - as a number
+            if (string.IsNullOrEmpty(txtfullwords.Text)){            
+                words = double.Parse(txtWords.Text);
+                if (words <= 20)
+                {
+                    result = 4.50;
+                    string formattedMoneyValue = String.Format("{0:C2}", result);
+                    lblResult.Text = "Cost of sending Telegram is " + formattedMoneyValue;
+                    lblResult.Visible = true;
+                }
+                else if (words > 20)
+                {
+                    result = (words - 20) * 0.17 + (4.50);
+                    string formattedMoneyValue = String.Format("{0:C2}", result);
+                    lblResult.Text = "Cost of sending Telegram is " + formattedMoneyValue;
+                    lblResult.Visible = true;
+                }
+            }
+            else
+            {
+                //calculate number of words in text that has been written
+                //words = double.Parse(txtfullwords.Text);
+                int numberofwords = txtfullwords.Text.Length;
+
+                if (numberofwords <= 20)
+                {
+                    result = 4.50;
+                    string formattedMoneyValue = String.Format("{0:C2}", result);
+                    lblResult.Text = "Cost of sending Telegram is " + formattedMoneyValue;
+                    lblResult.Visible = true;
+                }
+                else if (numberofwords > 20)
+                {
+                    result = (numberofwords - 20) * 0.17 + (4.50);
+                    string formattedMoneyValue = String.Format("{0:C2}", result);
+                    lblResult.Text = "Cost of sending Telegram is " + formattedMoneyValue;
+                    lblResult.Visible = true;
+                }
+            }
+        }
+
+        private void txtfullwords_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
